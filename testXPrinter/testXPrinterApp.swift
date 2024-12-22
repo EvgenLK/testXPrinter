25bf796b8c64a9f14ea17dc8a84b85bb23fcb25d
Kutasov Evgenii <kutasovel@icloud.com>
2024-12-22 19:01:23 +1000

    Initial Commit

---
+++ testXPrinter/testXPrinterApp.swift
@@ -0,0 +1,17 @@
+//
+//  testXPrinterApp.swift
+//  testXPrinter
+//
+//  Created by Evgenii on 22.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct testXPrinterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
